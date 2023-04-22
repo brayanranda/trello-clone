205be6caaca051b71deb91dc0430cb68be60d433
brayanismaelpr <brayanismaelpr@ufps.edu.co>
2023-04-22 07:42:00 -0500

    Home & Agregando fontawesome

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { faBox, faWaveSquare, faClock } from "@fortawesome/free-solid-svg-icons"
+import { faTrello } from "@fortawesome/free-brands-svg-icons"
 
 @Component({
   selector: 'app-boards',
   templateUrl: './boards.component.html',
 })
 export class BoardsComponent implements OnInit {
+
+  faBox=faBox
+  faClock=faClock
+  faTrello=faTrello
+  faWaveSquare=faWaveSquare
 
   constructor() { }
 
